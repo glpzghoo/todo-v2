46f742183e1d681221313ba85eadb875accf89f3
Nergui adiyakhuu <my.as.glpzghoo@gmail.com>
2025-04-01 02:34:11 +0800

    did something lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { cookies } from "next/headers";
 import SetCookieButton from "./components/setCookieButton";
+import axios from "axios";
+import { useEffect } from "react";
 // const jwtCookie =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkpzSk9Rc19TLWg5YXdHeXdJaldrVyIsInVzZXJuYW1lIjoiZ2xwemdob28iLCJpYXQiOjE3NDM0Mzg5MjksImV4cCI6MTc0MzQ0MjUyOX0.6X3lN59O2ksDPjT9OpyEDgg_Y8KeZ7igaESucwCK_jY";
 export default function Home() {
@@ -9,5 +12,14 @@ export default function Home() {
   //   const cookie = await cookies();
   //   const jwt = cookie.set("accessToken", jwtCookie);
   // };
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.post(
+        "https://todo-graphql-backend.glpzghoo.space"
+      );
+      console.log(res);
+    };
+    fetchData();
+  }, []);
   return <div className="min-h-screen"></div>;
 }
