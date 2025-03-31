0deea76d17c9466d2836cda4f33518966f559c46
Nergui adiyakhuu <my.as.glpzghoo@gmail.com>
2025-04-01 02:28:22 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-"use client";
-
+import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
+import SetCookieButton from "./components/setCookieButton";
+// const jwtCookie =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkpzSk9Rc19TLWg5YXdHeXdJaldrVyIsInVzZXJuYW1lIjoiZ2xwemdob28iLCJpYXQiOjE3NDM0Mzg5MjksImV4cCI6MTc0MzQ0MjUyOX0.6X3lN59O2ksDPjT9OpyEDgg_Y8KeZ7igaESucwCK_jY";
 export default function Home() {
-  return <div>still working on it :)</div>;
+  // const handleClick = async () => {
+  //   "use server";
+  //   const cookie = await cookies();
+  //   const jwt = cookie.set("accessToken", jwtCookie);
+  // };
+  return <div className="min-h-screen"></div>;
 }
