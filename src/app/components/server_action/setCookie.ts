29c66c904dd39f0135a785ab73f68104b3d1ae97
Nergui adiyakhuu <my.as.glpzghoo@gmail.com>
2025-04-03 21:13:06 +0800

    done

--- src/app/components/server_action/setCookie.ts
+++ src/app/components/server_action/setCookie.ts
@@ -6,8 +6,6 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function setCookie(loginResponse: loginResponse) {
   if (loginResponse) {
     const cookie = await cookies();
-    cookie.set("accessTokenTodo", loginResponse.loginUser.JWT, {
-      maxAge: 60 * 60,
-    });
+    cookie.set("accessTokenTodo", loginResponse.loginUser.JWT);
   }
 }
