4ce30aa41663be7060d10ccc2f7c61dca986cb31
Nergui adiyakhuu <my.as.glpzghoo@gmail.com>
2025-04-03 21:44:01 +0800

    mhmmmmmmmmmmmmmmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,7 +143,6 @@ export default function Home() {
       setWaiting(false);
     }
   };
-  // console.log(form);
   return loading ? (
     <div className="fixed transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <Loading />
